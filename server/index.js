dd1f734eaf2e98ae1b244ba765ca73557904a7e3
Andrew Corry <alcorry112@outlook.com>
2022-02-24 14:32:08 -0600

    Rollbar added

--- server/index.js
+++ server/index.js
@@ -11,7 +11,16 @@ app.get('/', function(req,res){
 });
 
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'cdad3cb0130046a58e34ec3c265248d8',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 
 const port = process.env.PORT || 4555;
